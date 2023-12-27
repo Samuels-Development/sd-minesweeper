1b895469684212fd4240b1e1ae46e42d07bdecd5
Samuel Nicol <99494967+Github-Samuel@users.noreply.github.com>
2023-12-27 18:15:46 +0100

    Update sv_config.lua

--- sv_config.lua
+++ sv_config.lua
@@ -3,5 +3,5 @@ Config = {}
 Config.DirtyCash = false -- false = you want to use regular cash instead of dirty cash, true = you want to use dirty_money (markedbills)
 Config.MetaData = false -- if dirtycash true and this true, then you'll be given 'markedbills' with 'worth' attached to them (works with qb-inventory and all of it's forks (eg. ps-inventory etc.))
 
-Config.Items = { 'laptop', 'security_card_01', 'security_card_02' } -- Items that are allowed to to be given in the AddItem event (from the golden box tile)
+Config.Items = { 'laptop', 'security_card_01', 'security_card_02' } -- Items that are allowed to to be given in the AddItem event (if the goldenbox was uncovered)
 Config.MaxMoney = { Enable = false, Amount = 10000 } -- Max amount of money that can be given to a player from completing the minigame if Config.MaxMoney.Enable is true.
